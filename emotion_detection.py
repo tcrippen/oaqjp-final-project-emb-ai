1e63c9042cb0aa8be609708d1b21c39479c0c91c
tcrippen <139709380+tcrippen@users.noreply.github.com>
2026-03-05 11:09:25 -0500

    Add emotion detection package and assignment files

---
+++ emotion_detection.py
@@ -0,0 +1,21 @@
+import requests
+
+def emotion_detector(text_to_analyse):
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+
+    input_json = {
+        "raw_document": {
+            "text": text_to_analyse
+        }
+    }
+
+    response = requests.post(url, json=input_json, headers=headers)
+
+    if response.status_code != 200:
+        return None
+
+    return response.json()
